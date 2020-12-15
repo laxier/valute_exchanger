089783de1ad6f374da32696e597be1fd06bc79f9
Kirill Timofeev <k788364@gmail.com>
2020-12-15 22:09:17 +0400

    added my code
    forget about upploading it

---
+++ Converter.py
@@ -0,0 +1,24 @@
+#CurrencyConverter.py
+#Scrypted by: Laxier
+import json
+import requests
+def first(dol):
+    # gets RUB from USD
+    return(dol * float(rate["RUB"]) / float(rate["USD"]))
+def second(rub):
+    # gets USD from RUB
+    return(rub / float(rate["RUB"]) * float(rate["USD"]))
+response = requests.get("https://api.exchangeratesapi.io/latest")
+rate = response.json()['rates']
+print(response.json())
+print("Hello! This is a Currency Converter")
+print("What wold you like to convert?")
+print("1 - dollar to rubble")
+print("2 - rubble to dollar")
+choice = input("> ")
+currency = float(input("enter the ammount of money> "))
+if(choice == "1"):
+    print("{:.2f} RUB".format(first(currency)))
+elif(choice == "2"):
+    print("{:.2f} USD".format(second(currency)))
+else: print("ERROR please try again.")
